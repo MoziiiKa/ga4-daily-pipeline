455373d1c40e8f51bb8aafe7a03424987ca555a8
moziiii <562220@student.saxion.nl>
2025-05-08 10:25:41 +0200

    ♻️  refactor sql files

--- sql/views/v_descriptive.sql
+++ sql/views/v_descriptive.sql
@@ -1,10 +1,11 @@
 CREATE OR REPLACE VIEW `Mozaffar_Kazemi_GA4Model.Mozaffar_Kazemi_DescriptiveV` AS
 SELECT
   PARSE_DATE('%Y%m%d', CAST(event_date AS STRING)) AS event_date,
-  COUNTIF(event_name = 'page_view')       AS page_views,
-  COUNT(DISTINCT user_pseudo_id)          AS active_users,
-  APPROX_QUANTILES(event_value_in_usd, 2)[OFFSET(1)] AS median_value_usd
-FROM
-  `crystalloids-candidates.Mozaffar_Kazemi_GA4Raw.Mozaffar_Kazemi_DailyEvents`
-GROUP BY
-  event_date;
+  COUNTIF(event_name = 'page_view') AS page_views,
+  COUNT(DISTINCT user_pseudo_id)  AS active_users,
+  COALESCE(
+    APPROX_QUANTILES(event_value_in_usd, 2)[OFFSET(1)],
+    0
+  ) AS median_value_usd
+FROM `crystalloids-candidates.Mozaffar_Kazemi_GA4Raw.Mozaffar_Kazemi_DailyEvents`
+GROUP BY event_date;
