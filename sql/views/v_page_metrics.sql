787d9d22c9f87f57e524c4dc34e7113aa3e2b5e6
moziiii <562220@student.saxion.nl>
2025-05-08 10:02:39 +0200

    ♻️  refactor sql files

--- sql/views/v_page_metrics.sql
+++ sql/views/v_page_metrics.sql
@@ -1,10 +1,16 @@
 CREATE OR REPLACE VIEW `Mozaffar_Kazemi_GA4Model.Mozaffar_Kazemi_PageMetricsV` AS
 WITH flat AS (
   SELECT
-    _PARTITIONDATE AS event_date,
-    (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'page_title') AS page_title,
-    (SELECT value.int_value   FROM UNNEST(event_params) WHERE key = 'page_view')  AS page_views
-  FROM  `crystalloids-candidates.Mozaffar_Kazemi_GA4Raw.Mozaffar_Kazemi_DailyEvents`
+    PARSE_DATE('%Y%m%d', CAST(event_date AS STRING)) AS event_date,
+    (SELECT value.string_value
+       FROM UNNEST(event_params)
+      WHERE key = 'page_title')                AS page_title,
+    (SELECT value.int_value
+       FROM UNNEST(event_params)
+      WHERE key = 'page_view')                 AS page_views
+  FROM `crystalloids-candidates.Mozaffar_Kazemi_GA4Raw.Mozaffar_Kazemi_DailyEvents`
   WHERE event_name = 'page_view'
 )
-SELECT * FROM flat WHERE page_title IS NOT NULL;
+SELECT *
+FROM flat
+WHERE page_title IS NOT NULL;
