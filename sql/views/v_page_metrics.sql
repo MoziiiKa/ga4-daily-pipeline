24fb1a2de33c82b038b39e4197a8549c04f82f0c
moziiii <562220@student.saxion.nl>
2025-05-08 10:08:16 +0200

    ♻️  refactor sql files

--- sql/views/v_page_metrics.sql
+++ sql/views/v_page_metrics.sql
@@ -1,16 +1,37 @@
 CREATE OR REPLACE VIEW `Mozaffar_Kazemi_GA4Model.Mozaffar_Kazemi_PageMetricsV` AS
 WITH flat AS (
   SELECT
+    -- Parse YYYYMMDD int to DATE
     PARSE_DATE('%Y%m%d', CAST(event_date AS STRING)) AS event_date,
-    (SELECT value.string_value
-       FROM UNNEST(event_params)
-      WHERE key = 'page_title')                AS page_title,
-    (SELECT value.int_value
-       FROM UNNEST(event_params)
-      WHERE key = 'page_view')                 AS page_views
-  FROM `crystalloids-candidates.Mozaffar_Kazemi_GA4Raw.Mozaffar_Kazemi_DailyEvents`
-  WHERE event_name = 'page_view'
+
+    -- Extract page_title from the JSON array string
+    (SELECT
+       JSON_EXTRACT_SCALAR(elem, '$.value.string_value')
+     FROM UNNEST(
+       JSON_EXTRACT_ARRAY(event_params, '$')
+     ) AS elem
+     WHERE JSON_EXTRACT_SCALAR(elem, '$.key') = 'page_title'
+    ) AS page_title,
+
+    -- Extract page_views (int) and cast it
+    CAST(
+      (SELECT
+         JSON_EXTRACT_SCALAR(elem, '$.value.int_value')
+       FROM UNNEST(
+         JSON_EXTRACT_ARRAY(event_params, '$')
+       ) AS elem
+       WHERE JSON_EXTRACT_SCALAR(elem, '$.key') = 'page_view'
+      ) AS INT64
+    ) AS page_views
+
+  FROM
+    `crystalloids-candidates.Mozaffar_Kazemi_GA4Raw.Mozaffar_Kazemi_DailyEvents`
+  WHERE
+    event_name = 'page_view'
 )
-SELECT *
-FROM flat
-WHERE page_title IS NOT NULL;
+SELECT
+  *
+FROM
+  flat
+WHERE
+  page_title IS NOT NULL;
