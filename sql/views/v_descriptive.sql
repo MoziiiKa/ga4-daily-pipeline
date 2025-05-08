bb3a092b2ecb8a619c6eaf5bc7fcc74749117d15
moziiii <562220@student.saxion.nl>
2025-05-08 09:51:24 +0200

    ♻️  sql files

--- sql/views/v_descriptive.sql
+++ sql/views/v_descriptive.sql
@@ -1,8 +1,10 @@
 CREATE OR REPLACE VIEW `Mozaffar_Kazemi_GA4Model.Mozaffar_Kazemi_DescriptiveV` AS
 SELECT
-  _PARTITIONDATE                      AS event_date,
+  CAST(event_date AS DATE)      AS event_date,
   COUNTIF(event_name = 'page_view')   AS page_views,
   COUNT(DISTINCT user_pseudo_id)      AS active_users,
   APPROX_QUANTILES(event_value_in_usd, 2)[OFFSET(1)] AS median_value_usd
-FROM   `crystalloids-candidates.Mozaffar_Kazemi_GA4Raw.Mozaffar_Kazemi_DailyEvents`
-GROUP  BY event_date;
+FROM
+  `crystalloids-candidates.Mozaffar_Kazemi_GA4Raw.Mozaffar_Kazemi_DailyEvents`
+GROUP BY
+  event_date;   -- group by the real column
